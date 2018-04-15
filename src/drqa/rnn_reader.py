321a3788867b1785d02222f3075640beca508ffc
Sean <xz1757@nyu.edu>
2018-04-14 22:59:42 -0400

    f

--- src/drqa/rnn_reader.py
+++ src/drqa/rnn_reader.py
@@ -106,7 +106,7 @@ class RnnDocReader(nn.Module):
             question_hidden_size,
         )
 
-    def forward(self, x1, x1_f, x1_pos, x1_ner, x1_mask, x2, x2_mask, x1_charembed):
+    def forward(self, x1, x1_f, x1_pos, x1_ner, x1_mask, x2, x2_mask):
         """Inputs:
         x1 = document word indices             [batch * len_d]
         x1_f = document word features indices  [batch * len_d * nfeat]
